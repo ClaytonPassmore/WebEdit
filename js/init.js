97ccffd9a337d3a7cf851506e04c4310f1366083
unknown <ctpassmore@gmail.com>
2015-03-12 00:00:38 -0400

    Added ability to open a file. UI is extremely ugly

--- js/init.js
+++ js/init.js
@@ -7,7 +7,8 @@ require.config({
 
 define(["jquery",
 		"../CodeMirror/lib/codemirror",
-		"../require-css/css!../CodeMirror/lib/codemirror"],
+		"../require-css/css!../CodeMirror/lib/codemirror",
+		"../require-css/css!../Font-Awesome/css/font-awesome.min"],
 function($, CodeMirror) {
 
 	var Editor = {
@@ -17,10 +18,43 @@ function($, CodeMirror) {
 
 			this.$el.css("width", "100%")
 					.css("height", "100%");
-			this.$editor = CodeMirror(this.$el[0],
+			this.$menu_div = $("<div />").css("width", "100%")
+										 .css("height", "70px")
+										 .appendTo(this.$el);
+			var that = this;
+			this.$file_opener = $("<input />").attr("type", "file");
+			$(this.$file_opener).on("change", function(event) { that.read_file(event); });
+			var $open_button = $("<div />").appendTo(this.$menu_div)
+										   .css("border", "1px solid black")
+										   .css("padding", "10px")
+										   .css("float", "left")
+										   .css("margin", "10px")
+										   .css("cursor", "pointer")
+										   .click(function() {
+				that.$file_opener.click();
+			}).append($("<i />").attr("class", "fa fa-file"));
+			this.$editor_div = $("<div />").css("width", "100%")
+										   .css("height", "100%")
+										   .appendTo(this.$el);
+			this.editor = CodeMirror(this.$editor_div[0],
 				{lineWrapping: true,
 				 lineNumbers: true,
 				 autofocus: true});
+			this.editor.setSize("100%", "100%");
+		},
+		read_file: function(event) {
+			var fd = event.target.files[0];
+			if(fd) {
+				document.title = fd.name;
+				var that = this;
+				var reader = new FileReader();
+				reader.onload = function(evnt) {
+					that.editor.setValue(evnt.target.result);
+				}
+				reader.readAsText(fd);
+			} else {
+				alert("Failed to load file");
+			}
 		},
 	};
 
